15a26eb1a63ce03052f3f7e75853e5cb992391d0
“younthing” <fanxingfu3344@gmail.com>
2023-08-19 18:18:11 +0800

    feat: unitest class

--- functional_tests.py
+++ functional_tests.py
@@ -1,9 +1,49 @@
 """Module for functional tests."""
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
 
-assert "Congratulations" in browser.title
+class NewVisitorTest(unittest.TestCase):
+    """Test new visitor"""
 
-browser.quit()
+    def setUp(self):  # unittest 会在每个测试方法之前运行 setUp
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):  # unittest 会在每个测试方法之后运行 tearDown
+        self.browser.quit()
+
+    # unittest 会运行所有以 test_ 开头的方法
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """test can start a list and retrieve it later"""
+        # online to-do list
+        self.browser.get("http://localhost:8000")
+
+        # page title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
+
+        ## 应用邀请她输入一个代办事项
+
+        ## 她在一个文本框中输入了"Buy peacock feathers"
+        ## 她的爱好是使用假蝇做饵钓鱼
+
+        ## 她按回车键后,页面更新了
+        ## 代办事项表格中显示了"1: Buy peacock feathers"
+
+        ## 页面中又显示了一个文本框,可以输入其他的代办事项
+        ## 她输入了"Use peacock feathers to make a fly"
+        ## 她做事很有条理
+
+        ## 页面再次更新,她的清单中显示了这两个代办事项
+
+        ## 她想知道这个网站是否会记住她的清单
+        ## 她看到网站为她生成了一个唯一的URL
+        ## 而且页面中有一些文字解说这个功能
+
+        ## 她访问那个URL,发现她的代办事项列表还在
+
+        ## 她很满意,去睡觉了
+
+
+if __name__ == "__main__":
+    unittest.main(warnings="ignore")
