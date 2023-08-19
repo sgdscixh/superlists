31548639b724925f13d301d528ffab2c31f9456f
“younthing” <fanxingfu3344@gmail.com>
2023-08-20 04:14:01 +0800

    fix: Refactor assistant function

--- functional_tests.py
+++ functional_tests.py
@@ -15,6 +15,12 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):  # unittest 会在每个测试方法之后运行 tearDown
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """check for row in list table"""
+        table = self.browser.find_element("id", "id_list_table")
+        rows = table.find_elements("tag name", "tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
     # unittest 会运行所有以 test_ 开头的方法
     def test_can_start_a_list_and_retrieve_it_later(self):
         """test can start a list and retrieve it later"""
@@ -40,17 +46,19 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         time.sleep(1)
-        table = self.browser.find_element("id", "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
-        self.assertIn("1: Buy peacock feathers", [row.text for row in rows])
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         ## 页面中又显示了一个文本框,可以输入其他的代办事项
         ## 她输入了"Use peacock feathers to make a fly"
         ## 她做事很有条理
+        inputbox = self.browser.find_element("id", "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
 
-        self.fail("Finish the test!")
+        time.sleep(1)
         ## 页面再次更新,她的清单中显示了这两个代办事项
-
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
         ## 她想知道这个网站是否会记住她的清单
         ## 她看到网站为她生成了一个唯一的URL
         ## 而且页面中有一些文字解说这个功能
