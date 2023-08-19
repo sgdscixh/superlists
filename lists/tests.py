e06540360b3a88a7f7ebc1cc6747a0c8161b00cd
“younthing” <fanxingfu3344@gmail.com>
2023-08-19 21:18:00 +0800

    test: url match views

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,7 @@
 """ dummy test """
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
 
 
 class SmokeTest(TestCase):
@@ -8,3 +10,17 @@ class SmokeTest(TestCase):
     def test_bad_maths(self):
         """dummy test"""
         self.assertEqual(1 + 2, 3)
+
+
+class HomePageTest(TestCase):
+    """test home page"""
+
+    def test_root_url_resolves_to_home_page_view(self):
+        """test home page root url"""
+        found = resolve("/")
+        self.assertEqual(found.func, home_page)
+
+    # def test_uses_home_template(self):
+    #     """test home page"""
+    #     response = self.client.get("/")
+    #     self.assertTemplateUsed(response, "home.html")
