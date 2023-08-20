88200cfbbb9bb1a40582e55327a40044440fd84d
“younthing” <fanxingfu3344@gmail.com>
2023-08-20 16:53:36 +0800

    fix: clean test

--- lists/tests.py
+++ lists/tests.py
@@ -7,14 +7,6 @@ from lists.views import home_page
 from lists.models import Item
 
 
-class SmokeTest(TestCase):
-    """dummy test"""
-
-    def test_bad_maths(self):
-        """dummy test"""
-        self.assertEqual(1 + 2, 3)
-
-
 class HomePageTest(TestCase):
     """test home page"""
 
@@ -32,44 +24,24 @@ class HomePageTest(TestCase):
         self.assertIn("<title>To-Do lists</title>", html)
         self.assertTrue(html.strip().endswith("</html>"))
 
+    def test_only_saves_items_when_necessary(self):
+        """test only saves items when necessary"""
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
     def test_can_save_post_request(self):
         """test can save post request"""
 
         response = self.client.post("/", data={"item_text": "A new list item"})
-        self.assertIn("A new list item", response.content.decode())
-        self.assertTemplateUsed(response, "home.html")
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")
 
-
-class HomePageTest2(TestCase):
-    """test home page use reverse"""
-
-    def test_root_url_reverse_to_home_page_view(self):
-        """test home page root url"""
-        from django.urls import reverse  # reverse name to url
-
-        home_url = reverse("home")
-        found = resolve(home_url)
-        self.assertEqual(found.func, home_page)
-
-    def test_home_page_returns_correct_html(self):
-        """test home page html"""
-        request = HttpRequest()  # 用户请求网页时，django看到的是HttpRequest对象
-        response = home_page(request)
-        html = response.content.decode("utf8")
-        self.assertTrue(html.startswith("<html>"))
-        self.assertIn("<title>To-Do lists</title>", html)
-        self.assertTrue(html.strip().endswith("</html>"))
-
-    # def test_home_page_returns_correct_html2(self):
-    #     """test home page html use render_to_string"""
-
-    #     from django.template.loader import render_to_string
-
-    #     request = HttpRequest()
-    #     response = home_page(request)
-    #     html = response.content.decode("utf8")
-    #     expected_html = render_to_string("home.html")
-    #     self.assertEqual(html, expected_html)
+    def test_redirects_after_post(self):
+        """test redirects after post"""
+        response = self.client.post("/", data={"item_text": "A new list item"})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response["location"], "/")
 
 
 class ItemModelTest(TestCase):
--- lists/views.py
+++ lists/views.py
@@ -1,12 +1,14 @@
 """ views for lists app """
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
+
+from lists.models import Item
 
 
 # Create your views here.
-def home_page(request: HttpRequest) -> HttpResponse:
-    """home page"""
-
-    return render(
-        request, "home.html", {"new_item_text": request.POST.get("item_text", "")}
-    )
+def home_page(request):
+    """home page view"""
+    if request.method == "POST":
+        Item.objects.create(text=request.POST["item_text"])
+        return redirect("/")
+    return render(request, "home.html")
