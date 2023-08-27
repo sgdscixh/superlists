469847700e5ca7991e077b4ec9d4a387dde582d1
“younthing” <fanxingfu3344@gmail.com>
2023-08-27 15:21:08 +0800

    feat:  model net migrate

--- lists/models.py
+++ lists/models.py
@@ -1,4 +1,9 @@
+"""models for list app"""
 from django.db import models
+
+
+class List(models.Model):
+    """list model"""
 
 
 # Create your models here.
@@ -6,3 +11,4 @@ class Item(models.Model):
     """item model"""
 
     text = models.TextField(default="")
+    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
--- lists/views.py
+++ lists/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 # Create your views here.
@@ -19,5 +19,6 @@ def view_list(request: HttpRequest) -> HttpResponse:
 
 def new_list(request: HttpRequest) -> HttpResponse:
     """new list"""
-    Item.objects.create(text=request.POST["item_text"])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST["item_text"], list=list_)
     return redirect("/lists/the-only-list-in-the-world/")
